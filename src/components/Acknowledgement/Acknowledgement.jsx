a71b286a1ebe1e16a0b01188769cbf714a1e334e
Jijina KK <jijigopal2000@gmail.com>
2024-03-08 23:31:28 +0530

    created acknowledgement

---
+++ src/components/Acknowledgement/Acknowledgement.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Acknowledgement = () => {
+  return (
+    <>
+        <div className='issue-report'>
+        Thanks for bringing the issue to our attention.We'll review it shortly and provide an update soon!
+        </div>
+    </>
+  )
+}
