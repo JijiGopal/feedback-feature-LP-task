260aa980900add68734a32803ff7ae034471f066
Jijina KK <jijigopal2000@gmail.com>
2024-03-09 21:58:18 +0530

    paths added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-  
+
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import { BackToQuestions } from './components/BackToQuestions/BackToQuestions'
 import { ContactUs } from './components/ContactUs/ContactUs'
 import { FabBtn } from './components/FAB/FabBtn'
@@ -7,15 +9,23 @@ import { Feedback } from './components/Feedback/Feedback'
 import { Navbar } from './components/Navbar/Navbar'
 import { ReportIssues } from './components/ReportIssues/ReportIssues'
 import { Suggestion } from './components/Suggestions/Suggestion'
-import { LandingPage } from './pages/LandingPage'
+import { LandingPage } from './pages/LandingPage';
+
 function App() {
- 
+
   return (
     <>
+      <Router>
         <Navbar/>
         <BackToQuestions/>
-        <LandingPage/>
-         <FabBtn/>
+        <Routes>
+          <Route path='/' element={<LandingPage/>}/>
+          <Route path='/issue' element={<ReportIssues/>}/>
+          <Route path='/feedback' element={<Feedback/>}/>
+          <Route path='/suggestion' element={<Suggestion/>}/>
+          <Route path='/contact' element={<ContactUs/>}/>
+        </Routes>
+      </Router>
     </>
   )
 }
